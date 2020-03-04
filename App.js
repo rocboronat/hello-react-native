b0a4a2aedbdcf8c4302f96b4fb642fe92f789573
Roc Boronat <roc@fewlaps.com>
2020-03-04 15:44:51 +0100

    Let's translate words into sushi. I mean, pizza.

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'stretch' }}>
-        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue', alignSelf:'center' }} />
-        <View style={{ width: 50, backgroundColor: 'skyblue' }} />
-        <View style={{ width: 50, height: 200, backgroundColor: 'steelblue' }} />
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
-};
+}
